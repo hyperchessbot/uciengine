f9c6db40d4786021851cc87aa356b03cc3f8fd9c
hyperchessbot <hyperchessbot@gmail.com>
2021-01-05 14:37:56 +0000

    Go result

--- src/example.rs
+++ src/example.rs
@@ -2,13 +2,20 @@ use uciengine::uciengine::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let go_job = GoJob::new()		
+		.uci_opt("UCI_Variant".to_string(), "atomic".to_string())
+		.uci_opt("Hash".to_string(), "128".to_string())
+		.uci_opt("Threads".to_string(), "4".to_string())
+		.tc(Timecontrol::default())
+	;
+	
+	println!("go job {:?}", go_job);
+	
 	let mut uciengine = UciEngine::new("./stockfish12".to_string());
 	
-	let _ = uciengine.go().await?;
+	let result = uciengine.go(go_job).await?;
 	
-	std::thread::sleep(std::time::Duration::from_millis(5000));
-	
-	println!("done");
+	println!("result {:?}", result);
 	
 	Ok(())
 }
--- src/uciengine.rs
+++ src/uciengine.rs
@@ -3,7 +3,9 @@ use tokio::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};
 use std::process::Stdio;
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
+use std::collections::HashMap;
 
+/// uci engine
 #[derive(Debug)]
 pub struct UciEngine {
 	path: String,
@@ -11,6 +13,67 @@ pub struct UciEngine {
 	rx: Receiver<String>,
 }
 
+/// go command job
+#[derive(Debug)]
+pub struct GoJob {
+	uci_options: HashMap<String, String>,
+	go_options: HashMap<String, String>,
+}
+
+/// time control
+#[derive(Debug)]
+pub struct Timecontrol {
+	wtime: usize, winc: usize, btime: usize, binc: usize,
+}
+
+/// implementation of time control
+impl Timecontrol {
+	pub fn default() -> Timecontrol {
+		Timecontrol {
+			wtime: 60000, winc: 0, btime: 60000, binc: 0,
+		}
+	}
+}
+
+/// go command job implementation
+impl GoJob {
+	pub fn new() -> GoJob {
+		GoJob {
+			uci_options: HashMap::new(),
+			go_options: HashMap::new(),
+		}
+	}
+	
+	pub fn uci_opt(mut self, key:String, value:String) -> GoJob {
+		self.uci_options.insert(key, value);
+		
+		self
+	}
+	
+	pub fn go_opt(mut self, key:String, value:String) -> GoJob {
+		self.go_options.insert(key, value);
+		
+		self
+	}
+	
+	pub fn tc(mut self, tc: Timecontrol) -> GoJob {
+		self.go_options.insert("wtime".to_string(), format!("{}", tc.wtime));
+		self.go_options.insert("winc".to_string(),  format!("{}", tc.winc));
+		self.go_options.insert("btime".to_string(), format!("{}", tc.btime));
+		self.go_options.insert("binc".to_string(),  format!("{}", tc.binc));
+		
+		self
+	}
+}
+
+/// go command result
+#[derive(Debug)]
+pub struct GoResult {
+	bestmove: Option<String>,
+	ponder: Option<String>,
+}
+
+/// uci engine implementation
 impl UciEngine {
 	pub fn new(path: String) -> UciEngine {		
 		let mut cmd = Command::new(path.as_str());
@@ -74,20 +137,48 @@ impl UciEngine {
 	pub async fn issue_command(&mut self, command: String) -> Result<(), Box<dyn std::error::Error>> {
 		println!("issuing command {}", command);
 		
-		let result = self.stdin.write_all(command.as_bytes()).await?;
+		let result = self.stdin.write_all(format!("{}\n", command).as_bytes()).await?;
 		
 		println!("issue command result {:?}", result);
 
 		Ok(())
 	}
 	
-	pub async fn go(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-		let _ = self.issue_command("go depth 5\n".to_string()).await?;
+	pub async fn go(&mut self, go_job: GoJob) -> Result<GoResult, Box<dyn std::error::Error>> {
+		for (key, value) in go_job.uci_options {
+			self.issue_command(format!("setoption name {} value {}", key, value).to_string()).await?;
+		}
+		
+		let mut go_command = "go".to_string();
+		
+		for (key, value) in go_job.go_options {
+			go_command = go_command + &format!(" {} {}", key, value);
+		}
+		
+		let _ = self.issue_command(go_command).await?;
 		
 		let result = self.rx.recv();
 		
 		println!("go command result {:?}", result);
 		
-		Ok(())
+		let mut bestmove:Option<String> = None;
+		let mut ponder:Option<String> = None;
+		
+		if let Ok(result) = result {
+			let parts:Vec<&str> = result.split(" ").collect();
+		
+			if parts.len() > 1 {
+				bestmove = Some(parts[1].to_string());
+			}
+
+			if parts.len() > 3 {
+				ponder = Some(parts[3].to_string());
+			}
+		}
+		
+		Ok(GoResult {
+			bestmove: bestmove,
+			ponder: ponder,
+		})
 	}
 }
