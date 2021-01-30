80a2209f1a302f4e930c86bbcdcfeaa8cc321943
hyperchessbot <hyperchessbot@gmail.com>
2021-01-30 17:23:25 +0100

    Analysis info

---
+++ examples/analysis.rs
@@ -0,0 +1,11 @@
+use uciengine::analysis::*;
+
+fn main() {
+    let ai = AnalysisInfo::new();
+
+    println!("ai {:?}", ai);
+
+    let ps = ParsingState::Init;
+
+    println!("ps {:?}", ps);
+}
--- src/analysis.rs
+++ src/analysis.rs
@@ -1,7 +1,7 @@
 macro_rules! gen_str_buff {
 	($(#[$attr:meta] => $type:ident, $size:expr),*) => { $(
 	    #[$attr]
-	    #[derive(Debug, Clone, Copy)]
+	    #[derive(Clone, Copy)]
 		pub struct $type {
 			pub len: usize,
 			pub buff: [u8; $size],
@@ -44,10 +44,87 @@ macro_rules! gen_str_buff {
 		        write!(f, "{}", String::from(*self))
 		    }
 		}
+
+		impl std::fmt::Debug for $type {
+			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		        write!(f, "[{}: '{}']", stringify!($type), String::from(*self))
+		    }
+		}
 	)* }
 }
 
+const UCI_MAX_LENGTH: usize = 5;
+const UCI_TYPICAL_LENGTH: usize = 4;
+const MAX_PV_MOVES: usize = 10;
+const PV_BUFF_SIZE: usize = MAX_PV_MOVES * (UCI_TYPICAL_LENGTH + 1);
+
 gen_str_buff!(
 /// UciBuff
-=> UciBuff, 10
+=> UciBuff, UCI_MAX_LENGTH,
+/// PvBuff
+=> PvBuff, PV_BUFF_SIZE
 );
+
+/// score
+#[derive(Debug, Clone, Copy)]
+pub enum Score {
+    Cp(i32),
+    Mate(i32),
+}
+
+use Score::*;
+
+/// analysis info
+#[derive(Debug, Clone, Copy)]
+pub struct AnalysisInfo {
+    /// best move
+    bestmove: UciBuff,
+    /// ponder
+    ponder: UciBuff,
+    /// pv
+    pv: PvBuff,
+    /// depth
+    pub depth: usize,
+    /// nodes
+    pub nodes: u64,
+    /// time
+    pub time: usize,
+    /// nodes per second
+    pub nps: usize,
+    /// score ( centipawns or mate )
+    pub score: Score,
+}
+
+/// analysis info implementation
+impl AnalysisInfo {
+    /// create new analysis info
+    pub fn new() -> Self {
+        Self {
+            bestmove: UciBuff::new(),
+            ponder: UciBuff::new(),
+            pv: PvBuff::new(),
+            depth: 0,
+            nodes: 0,
+            time: 0,
+            nps: 0,
+            score: Cp(0),
+        }
+    }
+}
+
+/// parsing state
+#[derive(Debug)]
+#[allow(dead_code)]
+// TODO: make this pub(crate)
+pub enum ParsingState {
+    Init,
+    Unknown,
+    Depth,
+    Nodes,
+    Time,
+    Nps,
+    Score,
+    ScoreCp,
+    ScoreMate,
+    Pv,
+}
